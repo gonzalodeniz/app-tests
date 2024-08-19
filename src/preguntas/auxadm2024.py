5d7d16bd6ec380adce99761bc8af31c19565e019
Gonzalo Déniz Acosta <soygonzalodeniz@gmail.com>
2024-08-19 17:20:37 +0100

    Corrige respuestas correctas pregunta y añade nombre al tema.

--- src/preguntas/auxadm2024.py
+++ src/preguntas/auxadm2024.py
@@ -1,29 +1,97 @@
 cuestionario = {
-    "Tema 1": [
+  "Tema 1":{
+      "titulo":"El acto administrativo: Concepto, clases y elementos. Eficacia y validez de los actos    administrativos. Motivación, notificación y publicación. Nulidad y anulabilidad de los actos administrativos. Conversión, conservación y convalidación.", 
+      "preguntas": [
         {
-            "pregunta": "¿Cuál es la capital de Francia?",
-            "opciones": ["París", "Madrid", "Roma"],
-            "respuesta_correcta": 0
+          "pregunta": "De conformidad con lo establecido en el artículo 34 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, los actos administrativos que dicten las Administraciones Públicas, bien de oficio o a instancia del interesado, se producirán por el órgano competente:",
+          "opciones": [
+            "Ajustándose a los requisitos y al procedimiento establecido.",
+            "Ajustándose al procedimiento establecido solamente cuando sea a instancia de parte.",
+            "Ajustándose al procedimiento establecido solamente cuando sea de oficio."
+          ],
+          "respuesta_correcta": 0
         },
         {
-            "pregunta": "¿Qué planeta es conocido como el planeta rojo?",
-            "opciones": ["Marte", "Venus", "Júpiter"],
-            "respuesta_correcta": 0
-        },
-        # Más preguntas para el Tema 1
-    ],
-    "Tema 2": [
-        {
-            "pregunta": "¿Quién pintó la Mona Lisa?",
-            "opciones": ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh"],
-            "respuesta_correcta": 0
+          "pregunta": "De conformidad con el artículo 35.1 Ley 39/2015 del Procedimiento Administrativo Común de las Administraciones Públicas, serán motivados, con sucinta referencia de hechos y fundamentos de derecho:",
+          "opciones": [
+            "Los acuerdos de aplicación de la tramitación de emergencia.",
+            "Los actos que resuelvan procedimientos de revisión de oficio de disposiciones o actos administrativos, recursos administrativos y procedimientos de arbitraje y los que declaren su inadmisión.",
+            "Los actos que admitan pruebas propuestas por los interesados."
+          ],
+          "respuesta_correcta": 1
         },
         {
-            "pregunta": "¿En qué año llegó el hombre a la Luna?",
-            "opciones": ["1965", "1969", "1972"],
-            "respuesta_correcta": 1
+          "pregunta": "En virtud de lo establecido en el artículo 36.1 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, los actos administrativos se producirán:",
+          "opciones": [
+            "Por escrito a través de medios electrónicos, a menos que su naturaleza exija otra forma más adecuada de expresión y constancia.",
+            "Por escrito, a menos que su naturaleza exija otra forma más adecuada de expresión y constancia.",
+            "Por escrito a través de medios telemáticos, a menos que su naturaleza exija otra forma más adecuada."
+          ],
+          "respuesta_correcta": 0
         },
-        # Más preguntas para el Tema 2
-    ],
-    # Más temas
+        {
+          "pregunta": "A tenor del artículo 39.1 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas:",
+          "opciones": [
+            "Los actos de las Administraciones Públicas sujetos al Derecho Administrativo se presumirán válidos y producirán efectos desde la fecha en que se dicten, salvo que en ellos se disponga otra cosa.",
+            "Los actos de las Administraciones Públicas sujetos al Derecho Administrativo se presumirán válidos y producirán efectos desde la fecha en que se publiquen, salvo que en ellos se disponga otra cosa.",
+            "Son válidos solo cuando los confirman los tribunales."
+          ],
+          "respuesta_correcta": 0
+        },
+        {
+          "pregunta": "De acuerdo con el artículo 40.2 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, toda notificación deberá ser cursada:",
+          "opciones": [
+            "Dentro del plazo de quince días a partir de la fecha en que el acto haya sido dictado.",
+            "Dentro del plazo de diez días a partir de la fecha en que el acto haya sido dictado.",
+            "Dentro del plazo de cinco días a partir de la fecha en que el acto haya sido dictado."
+          ],
+          "respuesta_correcta": 1
+        },
+        {
+          "pregunta": "De acuerdo con el artículo 41.1 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, ¿cómo se practicarán las notificaciones?",
+          "opciones": [
+            "Preferentemente por escrito y mediante entrega directa y presencial.",
+            "Preferentemente por medios electrónicos y, en todo caso, cuando el interesado resulte obligado a recibirlas por esta vía.",
+            "Únicamente por medios electrónicos."
+          ],
+          "respuesta_correcta": 1
+        },
+        {
+          "pregunta": "A tenor del artículo 42.2 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, cuando la notificación se practique en el domicilio del interesado, de no hallarse presente éste en el momento de entregarse la notificación, podrá hacerse cargo de la misma cualquier persona mayor de:",
+          "opciones": [
+            "14 años que se encuentre en el domicilio y haga constar su identidad.",
+            "16 años que se encuentre en el domicilio y haga constar su identidad.",
+            "18 años que se encuentre en el domicilio y haga constar su identidad."
+          ],
+          "respuesta_correcta": 0
+        },
+        {
+          "pregunta": "De conformidad con lo establecido en el artículo 45.1 de la Ley 39/2015 de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, los actos administrativos serán objeto de publicación:",
+          "opciones": [
+            "Cuando así lo aconsejen razones de interés particular.",
+            "Cuando así lo establezcan las normas reguladoras de cada procedimiento o cuando lo aconsejen razones de interés público apreciadas por el órgano competente.",
+            "Cuando lo aconsejen razones de interés público apreciadas por el órgano superior al órgano competente."
+          ],
+          "respuesta_correcta": 1
+        },
+        {
+          "pregunta": "De acuerdo con lo dispuesto en el artículo 47.1 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, ¿Cuál de los siguientes actos administrativos son nulos de pleno derecho?",
+          "opciones": [
+            "Los dictados prescindiendo total y absolutamente del procedimiento legalmente establecido o de las normas que contienen las reglas esenciales para la formación de la voluntad de los órganos colegiados.",
+            "Los dictados prescindiendo de algún trámite no esencial del procedimiento legalmente establecido o de las normas que contienen las reglas esenciales para la formación de la voluntad de los órganos colegiados.",
+            "La realización de actuaciones administrativas fuera del tiempo establecido para ellas, aun cuando la naturaleza del término o plazo no sea esencial."
+          ],
+          "respuesta_correcta": 0
+        },
+        {
+          "pregunta": "De acuerdo con lo dispuesto en el artículo 49.1 de la Ley 39/2015, de 1 de octubre, del Procedimiento Administrativo Común de las Administraciones Públicas, la nulidad o anulabilidad de un acto administrativo:",
+          "opciones": [
+            "Implicará la de los sucesivos en el procedimiento, sean éstos independientes o no del primero.",
+            "No implicará la de los sucesivos en el procedimiento que sean independientes del primero.",
+            "Implicará la de los sucesivos en el procedimiento, aunque sean independientes del primero."
+          ],
+          "respuesta_correcta": 1
+        }
+    ]
+  }
 }
